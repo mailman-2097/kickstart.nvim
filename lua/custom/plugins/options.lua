93a72600ae1dfe3c1d7b8554be0cbceef7dded7a
Nishad Saithaly <nishad.ks@det.nsw.edu.au>
2025-03-08 07:49:20 +1100

    Added dictionary words to complete options

--- lua/custom/plugins/options.lua
+++ lua/custom/plugins/options.lua
@@ -5,7 +5,14 @@ vim.opt.spelllang = { 'en_au' }
 -- Set the dictionary for spell checking
 vim.opt.dictionary:append('/usr/share/dict/words')
 
-vim.opt.complete:append('kspell')
+-- "." - Words in the current buffer
+-- "w" - Words in other windows
+-- "b" - Words in all buffers
+-- "u" - Words in unloaded buffers
+-- "t" - Keywords in tags
+-- "s" - Keywords in dictionary
+-- "kspell" - Words in the 'spell' language
+vim.opt.complete:append('s,kspell')
 
 -- Set tab width to 4 spaces
 vim.opt.tabstop = 4
